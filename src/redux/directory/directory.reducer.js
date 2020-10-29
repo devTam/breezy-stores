f976216cccf567421ef5c24466479b96d478de3e
Teebreezy101 <tammy.batubo@gmail.com>
2020-10-29 03:34:13 +0100

    moved directory and shop data into its own reducer and created respective selectors

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
             title: 'hats',
             imageUrl: 'https://i.postimg.cc/nV3SNvPg/hats.jpg',
             id: 1,
-            linkUrl: 'hats'
+            linkUrl: 'shop/hats'
         },
         {
             title: 'jackets',
             imageUrl: 'https://i.postimg.cc/fR9H31RQ/jackets.jpg',
             id: 2,
-            linkUrl: ''
+            linkUrl: 'shop/jackets'
         },
         {
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/tQRP6HV/sneakers.jpg',
             id: 3,
-            linkUrl: ''
+            linkUrl: 'shop/sneakers'
         },
         {
             title: 'womens',
             imageUrl: 'https://i.postimg.cc/RhDKSVr5/womens.jpg',
             size: 'large',
             id: 4,
-            linkUrl: ''
+            linkUrl: 'shop/womens'
         },
         {
             title: 'mens',
             imageUrl: 'https://i.postimg.cc/3NN13cc6/mens.jpg',
             size: 'large',
             id: 5,
-            linkUrl: ''
+            linkUrl: 'shop/mens'
         },
     ]
 }
